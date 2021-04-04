0ce18dcd900f184966c11ca0767fea8422045a78
duzunov <dimitar.ouzounoff@gmail.com>
2021-04-04 21:06:18 +0300

    Renamed variables, the model is now usable

--- src/central-banker.clj
+++ src/central-banker.clj
@@ -5,9 +5,10 @@
 ;; for testing
 (def start-econ
   {
-   :i 5, :i* 0
-   :p 5, :p* 0
-   :u 5, :u* 0
+   :r 5,
+   :pi 2,
+   :u 5
+   :y 100, :ye 100
    :quarter 0
    :last-event "Business as usual"
    })
@@ -20,48 +21,60 @@
       (apply update econ rest)))
 
 (def events
- [{
+  [{
     :description "Business as usual"
     :function identity
-    :probability 0.83
+    :probability 0.90 ;;used to be 0.83
     },
    {
     :description "Oil crisis"
-    :function (event-effect :p + 10)
+    :function (event-effect :pi + 10)
     :probability 0.01
     },
    {
     :description "Stock market crash"
-    :function (comp (event-effect :p* - 4)
-                    (event-effect :u* + 10))
+    :function (comp (event-effect :pi - 4)
+                    (event-effect :y - 10))
     :probability 0.05
     },
    {
     :description "Real-estate boom"
-    :function (comp (event-effect :p* + 3)
-                    (event-effect :u* - 1))
-    :probability 0.05
+    :function (comp (event-effect :pi + 3)
+                    (event-effect :y + 5))
+    :probability 0.01
     },
    {
     :description "Productivity boom"
-    :function (comp (event-effect :p - 5)
-                    (event-effect :u - 5))
-    :probability 0.04
+    :function (comp (event-effect :pi - 5)
+                    (event-effect :ye + 10))
+    :probability 0.01
     },
    {
     :description "Natural disaster"
-    :function (comp (event-effect :p + 9)
+    :function (comp (event-effect :ye - 5)
+                    (event-effect :pi + 9)
                     (event-effect :u - 9))
     :probability 0.01
     },
    {
     :description "Income Tax Cut"
-    :function (event-effect :p + 4)
+    :function (comp (event-effect :y + 4)
+                    (event-effect :ye + 2))
     :probability 0.01
     }
    ])
 
-;; Rich Hickey made this function
+;; Rich Hickey made this function, so I'm including his notice
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ant sim ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;   Copyright (c) Rich Hickey. All rights reserved.
+;;
+;;   The use and distribution terms for this software are covered by the
+;;   Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+;;   which can be found in the file CPL.TXT at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;
+;;   You must not remove this notice, or any other, from this software.
 (defn- wrand 
   "given a vector of slice sizes, returns the index of a slice given a
   random spin of a roulette wheel with compartments proportional to
@@ -73,6 +86,7 @@
       (if (< r (+ (slices i) sum))
         i
         (recur (inc i) (+ (slices i) sum))))))
+
 ;; recur arg for primitive local: sum is not matching primitive, had: Object, needed: long
 ;; Auto-boxing loop arg: sum
 
@@ -85,19 +99,11 @@
        events))
 
 (comment
+  "Test the randomness of events"
   (do
     (use 'clojure.pprint)
-    (print-table (take 10 (repeatedly (fn [] (rand-event events))))))
+    (print-table (take 16 (repeatedly (fn [] (rand-event events))))))
   ,)
-
-;; update with weighted probability
-(defn event-update
-  "Weighted random event function applied to the economy, unpure print for debugging"
-  [econ]
-  (let [r (rand-event events)]
-    (println (:description r))
-    ;; Should I use tap> instead of this?
-    ((:function r) econ)))
 
 ;; update with weighted probability
 (defn event-update
@@ -113,38 +119,54 @@
 (defn set-policy []
   (Integer/parseInt (read-line)))
 
-;; Note that this model doesn't allow for deflation which is pretty rare anyway
-(defn model [econ i]
+(defn model [econ r]
   (letfn [(add-abs [o & rest]
             (let [res (apply + o rest)]
-              (if (neg? res) 0 res)))]
-    (-> econ
-        (update :p add-abs (:p* econ))
-        (update :u add-abs (:u* econ))
-        (update :p* - i)
-        (update :u* + i))))
+              (if (neg? res) 0 res)))
+          (is [r]
+            "returns the y₁; variant of the investment-saving curve from IS-LM"
+            (add-abs - 100 (* 0.25 r)))
+          (pc [pi r]
+            "returns the π₁, Phillips Curve variant"
+            (+ pi 5 (* 4 (- (is r) ye))))
+          (jobsearch [u pi]
+            (add-abs u (* -0.5 pi)))]
+    (let [{:keys [ye y pi]} econ]
+      (-> econ
+          (assoc :y (is r))
+          (update :pi pc r) ;; uses ye
+          (update :u jobsearch pi)
+          ;; add something for unemployment
+          ,))))
 
 (defn pass-quarter
-  "update the economy map with the events (DOTO functions) and policy"
-  [econ i]
+  "update the economy map with the events and policy"
+  [econ r]
   (-> econ
       event-update
-      (model i)
-      (update :i (fn [_ a] a) i)
-      (update :quarter inc)
-      ))
+      (model r)
+      (assoc :r r)
+      (update :quarter inc)))
+
+
+(defn game-report! [history]
+  (do
+    (println "Here is your game report:")
+    (clojure.pprint/print-table [:quarter :i :p :u :last-event] history)))
 
 ;; a variation of the Misery index by Arthur Okun
 ;; is used to calculate if you win the game
 
 (defn game-over [history]
   "See if the game is won or lost"
-  (let [p (map :p history)
+  (let [p (map :pi history)
         u (map :u history)
         avg (fn [coll] (/ (apply + 0.0 coll)
                           (count coll)))
         misery (+ (avg p)
                   (avg u))]
+    (game-report! history)
+    (println "Misery Index: " misery)
     (if  (<= misery 10.0)
       "You won the game!"
       "Game over, you lost!")))
@@ -155,28 +177,30 @@
   (def example-econ (atom start-econ))
   (swap! example-econ pass-quarter 0)
   (reset! example-econ start-econ)
-  ;; show 16 quarters in a table
+  ;; show 16 quarters saved in a var called hist in a table
   (do
-    (use 'clojure.pprint)
     (reset! example-econ start-econ)
-    (print-table (take 16 (repeatedly #(swap! example-econ pass-quarter 0)))))
-  
+    (def hist
+      (vec (take 16 (repeatedly #(swap! example-econ pass-quarter 5)))))
+    (clojure.pprint/print-table hist))
   ,)
 
 
-
+;; TODO:  use destructuring in let
+;; TODO: Separate presentation function
 (defn game
   "The game function waits for a policy (interest rate - i)
   to be applied in response to the state of the economy"
-  []
+  [length]
   (loop [econ start-econ, history []]
     (let [current-quarter (:quarter econ)]
       (println
-       "i:" (:i econ)
+       "r:" (:r econ)
        "Current quarter: " current-quarter
-       " Inflation: " (:p econ)
+       (:last-event econ) "\n"
+       " Inflation: " (:pi econ)
        " Unemployment: " (:u econ)
-       " enter your target i:")
-      (if (< current-quarter 3)
+       " enter your target r:")
+      (if (< current-quarter length)
         (recur (pass-quarter econ (set-policy)) , (conj history econ))
         (game-over history)))))
